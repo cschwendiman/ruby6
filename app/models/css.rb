a7f6c3b0bb139bb5d04d8c63803aa7cea32b0025
Troy Varney <troy.varney@att.net>
2013-03-06 19:57:35 -0600

    Template and CSS editing/downloading added.

--- app/models/css.rb
+++ app/models/css.rb
@@ -2,4 +2,8 @@ class Css < ActiveRecord::Base
 	has_attachment :max_size => 500.kilobytes, :storage => :file_system, :path_prefix => 'public/user_stylesheets', :content_type => 'text/css'
   	validates_as_attachment
   	belongs_to :game
+  
+  def get_public_filename
+    return File.join(Rails.public_path, public_filename)
+  end
 end
